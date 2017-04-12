1ff0d9200dce2b57b5e7b141d1c1e10dabeb2401
Your Name <you@example.com>
2017-04-12 15:22:46 +0530

    Modified pages

--- app1.js
+++ app1.js
@@ -1,17 +1,42 @@
 
-  $( document ).ready(function() {	  
+  $(document).ready(function() {	  
 	  
 		$("#ProfileTab").hide();
 		$("#FeedTab").hide();
-		
-		$('#profile a').click(function (e) {
+
+		$('#profilelink a').click(function (e) {
 			$("#FeedTab").hide();
-			$("#ProfileTab").show(); })
+			$("#ProfileTab").show(); 
+		    $("#feedlink" ).removeClass( "active" );
+		     $("#profilelink" ).addClass( "active" );
+		})		
 		
-		
-		$('#feed a').click(function (e) {
+		$('#feedlink a').click(function (e) {
 			$("#ProfileTab").hide();
-			$("#FeedTab").show(100); })
+			$("#FeedTab").show(100); 
+			 $("#profilelink" ).removeClass( "active" );
+			 $("#feedlink" ).addClass( "active" );
 
 
+		})
+ 
+  var myFacebookToken = 'EAACEdEose0cBAOHattDCoCQ1hgZBWZBGkZB3eqtcryqLBMZAhZBSdZB8sBY21C3pwoiOTg6WiZCJzUJvI32e8tklzptPxREsjJ14S04VkdSwt8CK4ZC3rKJI6liBem566CBuUqX3tY7aMnKkA7tKsKdM8pJETe5FxSY0PGN3z0920RQtic8gkL2u';
+
+
+ 	function getFacebookInfo(){
+
+        $.ajax('https://graph.facebook.com/me?access_token='+myFacebookToken,{
+                success : function(response){
+                   $("#name").text(response.name);
+                    $("#myEmail").text(response.email);
+                    $("#myProfileId").html('<a target="blank" href="https://facebook.com/'+response.id+'">https://facebook.com/'+response.id+'</a>');
+                    $("#myHomeTown").text(response.hometown.name);
+
+                }
+            }
+        );
+    }
+    $("#facebookBtn").on('click',getFacebookInfo)
+
+			
   });
